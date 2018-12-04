ad514edba0db68ff2d0e9d4b403feaa987161d2a
mountaingoat5 <35748667+mountaingoat5@users.noreply.github.com>
2018-12-04 10:17:54 -0700

    Add files via upload
    WhaleHouse component

---
+++ WhaleHouse.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const WhaleHouse = props =>(
+	<div>
+		<h1>{props.whaleObject[i].name} </h1>
+		<h2>{props.whaleObject[i].latin} </h2> 
+		<p>{props.whaleObject[i].about}	</p>
+		<p>{props.whaleObject[i].age} </p
+		<p>{props.whaleObject[i].size} </p>
+		<p>{props.whaleObject[i].amount} </p>
+		<img>{props.whaleObject[i].map} </img>		
+	</div>
+);
+
+
+
+
